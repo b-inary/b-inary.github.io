0e98dd06c3d112d7a5c401a2da60e57ef4d6cb46
Wataru Inariba <oinari17@gmail.com>
2022-08-06 23:54:09 +0900

    postflop_solver: update

--- postflop_solver/source-files.js
+++ postflop_solver/source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["postflop_solver"] = {"name":"","files":["bet_size.rs","game.rs","hand.rs","hand_table.rs","interface.rs","lib.rs","mutex_like.rs","range.rs","sliceop.rs","solver.rs","utility.rs"]};
+var sourcesIndex = JSON.parse('{\
+"postflop_solver":["",[],["bet_size.rs","game.rs","hand.rs","hand_table.rs","interface.rs","lib.rs","mutex_like.rs","range.rs","sliceop.rs","solver.rs","utility.rs"]]\
+}');
 createSourceSidebar();
