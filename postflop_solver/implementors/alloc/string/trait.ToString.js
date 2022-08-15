5f5b0c51596247a43c14caa0e6ca1f52f48f7a3c
Wataru Inariba <oinari17@gmail.com>
2022-08-16 00:57:04 +0900

    postflop_solver: update

--- postflop_solver/implementors/alloc/string/trait.ToString.js
+++ postflop_solver/implementors/alloc/string/trait.ToString.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["postflop_solver"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"postflop_solver/struct.Range.html\" title=\"struct postflop_solver::Range\">Range</a>","synthetic":false,"types":["postflop_solver::range::Range"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"postflop_solver":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"postflop_solver/struct.Range.html\" title=\"struct postflop_solver::Range\">Range</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- postflop_solver/implementors/postflop_solver/interface/trait.GameNode.js
+++ postflop_solver/implementors/postflop_solver/interface/trait.GameNode.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["postflop_solver"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"postflop_solver":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
