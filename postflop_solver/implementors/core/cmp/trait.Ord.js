ff37148fb0708eec9ff02d90f742d4016b32e5d3
Wataru Inariba <oinari17@gmail.com>
2022-11-29 21:02:38 +0900

    postflop_solver: update

--- postflop_solver/implementors/core/cmp/trait.Ord.js
+++ postflop_solver/implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"postflop_solver":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"postflop_solver/enum.Action.html\" title=\"enum postflop_solver::Action\">Action</a>"]]
+"postflop_solver":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"postflop_solver/enum.Action.html\" title=\"enum postflop_solver::Action\">Action</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"postflop_solver/enum.BoardState.html\" title=\"enum postflop_solver::BoardState\">BoardState</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
