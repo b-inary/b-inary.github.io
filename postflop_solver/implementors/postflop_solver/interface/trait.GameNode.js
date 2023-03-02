b4db7c3a5be51ef03e7d5180349452cf830ba89b
Wataru Inariba <oinari17@gmail.com>
2023-03-02 20:52:22 +0900

    postflop_solver: update

--- postflop_solver/implementors/postflop_solver/interface/trait.GameNode.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"postflop_solver":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
