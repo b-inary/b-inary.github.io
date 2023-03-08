8a9a3173c6b43bb2617dfb3ff37a0fc2a6b8a0af
Wataru Inariba <oinari17@gmail.com>
2023-03-09 08:37:11 +0900

    postflop_solver: update

---
+++ postflop_solver/implementors/postflop_solver/interface/trait.GameNode.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"postflop_solver":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- postflop_solver/source-files.js
+++ postflop_solver/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"postflop_solver":["",[["game",[],["base.rs","evaluation.rs","interpreter.rs","mod.rs","node.rs","serialization.rs"]]],["action_tree.rs","alloc.rs","bet_size.rs","card.rs","hand.rs","hand_table.rs","interface.rs","lib.rs","mutex_like.rs","range.rs","sliceop.rs","solver.rs","utility.rs"]]\
+"postflop_solver":["",[["game",[],["base.rs","evaluation.rs","interpreter.rs","mod.rs","node.rs","serialization.rs"]]],["action_tree.rs","alloc.rs","atomic_float.rs","bet_size.rs","bunching.rs","card.rs","hand.rs","hand_table.rs","interface.rs","lib.rs","mutex_like.rs","range.rs","sliceop.rs","solver.rs","utility.rs"]]\
 }');
 createSourceSidebar();
